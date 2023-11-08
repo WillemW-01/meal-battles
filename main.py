c4b6f3b7bf10c3efc02e025bbb6eb5aff8c78ebd
WillemW-01 <daeben72@gmail.com>
2023-11-09 00:19:30 +0200

    Added game state class

--- main.py
+++ main.py
@@ -54,8 +54,10 @@ from classes.fruit import Fruit
 from classes.dairy import Dairy
 from classes.grain import Grain
 from classes.vegetable import Vegetable
+from game import Game
 import constants
 import custom_scanner
+import utils
 
 from colors import print_colors
 
@@ -70,7 +72,6 @@ CLASSES = {
     "Grain": Grain,
     "Fruit": Fruit,
 }
-FOOD_TABLE = {}
 OUTPUT_WIDTH = 62
 
 """
@@ -112,8 +113,8 @@ def build_decks(deck_filepath: str) -> dict[int, list[Food]]:
         for player_number, player_deck in deck_file.items():
             curr_color = "B" if curr_player == 0 else "R"
             for food_item in player_deck:
-                parent_class = get_parent_class(food_item)
-                stats = FOOD_TABLE[parent_class][food_item]
+                parent_class = game.get_parent_class(food_item)
+                stats = game.from_table(parent_class, food_item)
 
                 food_obj = generate_food_object(
                     parent_class, food_item, stats, curr_color
@@ -137,7 +138,7 @@ def build_decks(deck_filepath: str) -> dict[int, list[Food]]:
 
 
 def ask_player_index(player):
-    color = colored_player(player)
+    color = utils.colored_player(player)
     input_str = input(templates["ASK_OWN_CARD"] % (color, len(decks[player])))
     return int(input_str) - 1 if input_str != "" else None
 
@@ -160,33 +161,8 @@ def ask_should_skill():
 # region
 
 
-def get_parent_class(item):
-    for group, values in FOOD_TABLE.items():
-        for food in values:
-            if item == food:
-                return group
-    return None
-
-
 def generate_food_object(parent_class, food_name, stats, color):
     return CLASSES[parent_class](food_name, stats, color)
-
-
-def get_player_color(player):
-    return print_colors["B"] if player == 0 else print_colors["R"]
-
-
-def colored_player(player):
-    return f"{get_player_color(player)}Player {player+1}{print_colors[None]}"
-
-
-def flip_coin():
-    if random() < 0.5:
-        print(f"Flipped heads! {colored_player(0)} starts.")
-        return 0
-    else:
-        print(f"Flipped tails! {colored_player(1)} starts.")
-        return 1
 
 
 # endregion
@@ -206,7 +182,7 @@ def draw_round_num(round_num):
 
 def draw_table_player_name(player):
     width = OUTPUT_WIDTH - 9
-    print(f"{' ' * (width // 2)}{colored_player(player)}{' ' * (width // 2)}")
+    print(f"{' ' * (width // 2)}{utils.colored_player(player)}{' ' * (width // 2)}")
 
 
 def draw_table_heading_top():
@@ -262,40 +238,41 @@ if __name__ == "__main__":
         stats_file = argv[1]
         decks_file = argv[2]
 
+    game = Game()
+
     # Reading in the stats of all food items
-    FOOD_TABLE = read_stats(stats_file)
+    food_table = read_stats(stats_file)
+    game.load_food_table(food_table)
 
     # Reading in the players' decks
     decks = build_decks(decks_file)
+    game.load_decks(decks)
 
     # Start game play loop
 
     print("Flipping coin to decide who starts...")
-    curr_player = flip_coin()
+    game.choose_player()
 
-    game_over = False
-    round_num = 1
-    while not game_over:
-        draw_round_num(round_num)
+    while True:
+        # drawing phase
+        draw_round_num(game.get_round())
+        draw_decks(game.get_decks())
 
-        draw_decks(decks)
-
-        opponent = not curr_player
-
-        index_own = ask_player_index(curr_player)
+        # input phase
+        index_own = ask_player_index(game.get_player())
         if index_own is None:
             print("Player skipped round.")
-            curr_player = not curr_player
-            round_num += 1
+            game.next_round()
             continue
 
-        index_opp = ask_opp_index(curr_player)
+        index_opp = ask_opp_index(game.get_player())
         should_skill = ask_should_skill()
         print()
 
-        card_own = decks[curr_player][index_own]
-        card_opp = decks[opponent][index_opp]
+        card_own = decks[game.get_player()][index_own]
+        card_opp = decks[game.get_opponent()][index_opp]
 
+        # action phase
         if should_skill:
             card_own.special_ability()
             # activate its ability
@@ -304,12 +281,11 @@ if __name__ == "__main__":
         if index_opp is not None:
             is_killed = card_own.damage(card_opp)
             if is_killed:
-                decks[opponent].pop(index_opp)
+                decks[game.get_opponent()].pop(index_opp)
 
         # win condition
-        if len(decks[opponent]) == 0:
-            print(f"Game is finished! Player {int(opponent)} lost!")
+        if len(decks[game.get_opponent()]) == 0:
+            print(f"Game is finished! Player {int(game.get_opponent())} lost!")
             exit(0)
 
-        curr_player = not curr_player
-        round_num += 1
+        game.next_round()
---
+++ utils.py
@@ -0,0 +1,14 @@
+from colors import print_colors
+from random import random
+
+
+def get_player_color(player):
+    return print_colors["B"] if player == 0 else print_colors["R"]
+
+
+def colored_player(player):
+    return f"{get_player_color(player)}Player {player+1}{print_colors[None]}"
+
+
+def flip_coin():
+    return random() > 0.5
